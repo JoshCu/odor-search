a89b9cc07b31d32eee3646b0fc85286a0c61d956
JoshCu <Josh.Cu@gmail.com>
2023-03-12 15:02:11 -0400

    add basic search

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
+import requests
 import os
-
+import json
+import ast
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
@@ -10,26 +13,23 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        term = request.form["search_term"]
+        search_result = search_all(term)
+        return redirect(url_for("index", result=search_result))
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    search_result = request.args.getlist("result")
+    search_result = [ast.literal_eval(x) for x in search_result]
+
+    return render_template("index.html", result=search_result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def search_all(term):
+    # load the environment variables
+    atlas_api_key = os.environ.get("ATLAS_API_KEY")
+    url = "https://data.mongodb-api.com/app/data-dpfbx/endpoint/odor_search"
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    payload = {"search_term": term}
+    headers = {'api-key': atlas_api_key}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return json.loads(response.text)
